0742c9a71349b8c524f05d1286f629ab5998947f
Ronnie Hoogerwerf <rohooger@gmail.com>
2016-01-09 17:15:05 -0500

    First dump of code into github

--- pprofile_ext/util.py
+++ pprofile_ext/util.py
@@ -1,1 +1,20 @@
-__author__ = 'ronnie'
+import collections
+
+
+def update(d, u):
+    """
+    Perform a recursive update of dictionary `d` with the dictionary `u`
+
+    :param d: dictionary
+    :param u: dictionary
+
+    :return: updated dictionary
+    """
+    for k, v in u.iteritems():
+        if isinstance(v, collections.Mapping):
+            r = update(d.get(k, {}), v)
+            d[k] = r
+        else:
+            d[k] = u[k]
+
+    return d
